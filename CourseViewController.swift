89d1cf7c14ac1107efcc99ca1bc43f2d6f4f3a9e
wamuni <boshi.pan@outlook.com>
2024-04-09 10:43:42 +0100

    1. write viewcontroller for course 2. add UITableView to viewController 3. using default UITableViewCell with identifier "cell" 4. call datafetchable protocol instance to fetch course name 5. map the name to course 6. using DispatchQueue.main.async to reload data in table view 7. implement table view delegate and datasource protocol

---
+++ CourseViewController.swift
@@ -0,0 +1,66 @@
+//
+//  CourseViewController.swift
+//  DIUIKit
+//
+//  Created by 潘博石 on 09/04/2024.
+//
+
+import UIKit
+
+protocol DataFetchable {
+    func fetchCoursesName(completion: @escaping (([String]) -> Void))
+}
+
+struct Course {
+    let name: String
+}
+
+class CourseViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    let dataFetchable: DataFetchable
+    var courses: [Course] = []
+    
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
+    public init(dataFetchable: DataFetchable) {
+        self.dataFetchable = dataFetchable
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        view.backgroundColor = .systemBackground
+        dataFetchable.fetchCoursesName { names in
+            self.courses = names.map { Course(name: $0) }
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = courses[indexPath.row].name
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return courses.count
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+}
---
+++ DIUIKit.h
@@ -0,0 +1,18 @@
+//
+//  DIUIKit.h
+//  DIUIKit
+//
+//  Created by 潘博石 on 09/04/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DIUIKit.
+FOUNDATION_EXPORT double DIUIKitVersionNumber;
+
+//! Project version string for DIUIKit.
+FOUNDATION_EXPORT const unsigned char DIUIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DIUIKit/PublicHeader.h>
+
+
